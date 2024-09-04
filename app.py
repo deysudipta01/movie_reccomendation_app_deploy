6f351c6d71fdc6a71b57bd4fddcbd462dc6f27b8
Sudipta <sudiptadey877@gmail.com>
2024-09-04 20:50:11 +0530

    Update

--- app.py
+++ app.py
@@ -16,13 +16,9 @@ app = Flask(__name__)
 def home():
     return render_template("index.html")
 
-@app.route("/about")
-def about():
-    return render_template("about.html")
 
-@app.route("/contact")
-def contact():
-    return render_template("contact.html")
+
+
 
 
 
